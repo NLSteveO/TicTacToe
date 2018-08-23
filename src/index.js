7e3784824c3952baa43d8c35d18a5bf62d906c86
Steve O'Keefe <steveokeefe88@gmail.com>
2018-08-22 22:51:13 -0230

    Tutorial Wrapping Up: Use Two Loops To Create Board
    Rewrite Board to use two loops to make the squares instead of hardcoding them.

--- src/index.js
+++ src/index.js
@@ -42,23 +42,24 @@ class Board extends React.Component {
   }
 
   render() {
+    const rows = [];
+    for (let i = 0; i <= 6; i += 3) {
+      const squares = [];
+      for (let j = i; j < i + 3; j++) {
+        squares.push(this.renderSquare(j));
+      }
+      rows.push(
+        <div
+          key={i}
+          className="board-row"
+        >
+          {squares}
+        </div>
+      );
+    }
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {rows}
       </div>
     );
   }
